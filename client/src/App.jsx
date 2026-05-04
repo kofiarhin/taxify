1770714fae588ccfc3faad72feb48cd8085c3e82
kofi arhin <kofiarhin69@gmail.com>
2026-05-04 07:58:51 +0100

    test deployment fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Providers } from "./redux/providers";
 import { AppRouter } from "./routes/AppRouter";
 
+// main app
 function App() {
   return (
     <Providers>
